bae18bd01d6b13f900dd921e6efe6037afe87194
ywvv <ywvvvw@gmail.com>
2023-04-30 22:37:42 +0300

    add expression result

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,12 +3,45 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.calculator.databinding.ActivityMainBinding
+import com.ezylang.evalex.Expression
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val buttonsList = listOf(
+            binding.bZero,
+            binding.bOne,
+            binding.bTwo,
+            binding.bThree,
+            binding.bFour,
+            binding.bFive,
+            binding.bSix,
+            binding.bSeven,
+            binding.bEight,
+            binding.bSeven,
+            binding.bPoint,
+            binding.bPlus,
+            binding.bMinus,
+            binding.bMultiply
+        )
+
+        val numberStringBuilder = StringBuilder()
+
+        buttonsList.forEach { button->
+            button.setOnClickListener {
+                numberStringBuilder.append(button.text)
+                binding.tvResult.text = numberStringBuilder
+            }
+        }
+
+        binding.bEqual.setOnClickListener {
+            val expression = Expression(binding.tvResult.text.toString())
+            binding.tvResult.text = expression.evaluate().numberValue.toString()
+            numberStringBuilder.clear()
+        }
     }
 }
