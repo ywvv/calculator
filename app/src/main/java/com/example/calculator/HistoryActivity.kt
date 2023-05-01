d8394b6fe71c6d4b989d595c04e5c2a8675ed8be
ywvv <ywvvvw@gmail.com>
2023-05-01 21:53:42 +0300

    add arrayAdapter

--- app/src/main/java/com/example/calculator/HistoryActivity.kt
+++ app/src/main/java/com/example/calculator/HistoryActivity.kt
@@ -2,15 +2,20 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.ArrayAdapter
+import com.example.calculator.databinding.ActivityHistoryBinding
 
 class HistoryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHistoryBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_history)
+        binding = ActivityHistoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val historyList = intent.getStringArrayExtra("history_list") ?: arrayOf()
 
-        Log.d("MyLog", "${historyList.size}")
+        val itemsAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, historyList)
+
+        binding.lvHistory.adapter = itemsAdapter
     }
 }
